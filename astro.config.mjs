9729806654aba03144b5bb22591f1387ea2afa26
Ivan Ivanov <me@ivanov.pro>
2024-11-15 21:09:01 +0200

    update page and add some content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://emmalinux.github.io',
     compressHTML: true,
-    integrations: [
-        tailwind()
-    ]
+    integrations: [tailwind(), icon()]
 });
